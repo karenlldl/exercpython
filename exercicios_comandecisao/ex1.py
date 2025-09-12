589cc470242c5593a05f9c4ee0fff09c0a1f83df
Karen <karenlldlira@gmail.com>
2025-09-12 16:26:14 -0300

    new exercises

---
+++ exercicios_comandecisao/ex1.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que receba um número e mostre uma mensagem caso este número
+# seja maior que 10.
+
+num = int(input ("Digite um número: "))
+
+if num > 10:
+    print("O número é maior que 10")
+else:
+    print("O número é menor que 10")
